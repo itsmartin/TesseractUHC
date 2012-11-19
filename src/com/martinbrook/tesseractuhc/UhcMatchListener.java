406df007f0b4df213b1c5d18ce4a38349a04da0d
itsmartin <github@martinbrook.com>
2012-11-19 15:35:38 +0000

    Cancel interactions by ops. Make chest opening silent. Fixes #21

--- src/com/martinbrook/tesseractuhc/UhcMatchListener.java
+++ src/com/martinbrook/tesseractuhc/UhcMatchListener.java
@@ -5,7 +5,9 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -16,11 +18,15 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.DoubleChestInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import org.bukkit.ChatColor;
@@ -161,12 +167,7 @@ public class UhcMatchListener implements Listener {
 		}
 	}
 	
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent e) {
-		if (e.getBlock().getType() == Material.STONE) {
-			m.doMiningFatigue(e.getPlayer(), e.getBlock().getLocation().getBlockY());
-		}
-	}
+
 	
 	@EventHandler
 	public void onInteractEntityEvent(PlayerInteractEntityEvent e) {
@@ -237,4 +238,48 @@ public class UhcMatchListener implements Listener {
 		m.sendNotification(new DamageNotification(up, e.getDamage(), e.getCause(), e.getDamager()));
 		
 	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent e) {
+		// Mining fatigue
+		if (e.getBlock().getType() == Material.STONE) {
+			m.doMiningFatigue(e.getPlayer(), e.getBlock().getLocation().getBlockY());
+		}
+	}
+	
+	
+	/**
+	 * Prevent ops from interacting with anything when match is in progress.
+	 * 
+	 */
+	@EventHandler(ignoreCancelled = true)
+	public void onPlayerInteract(PlayerInteractEvent e) {
+		if (e.getPlayer().isOp() && m.getMatchPhase() == MatchPhase.MATCH) {
+
+			// Handle right-clicks on inventory blocks
+			if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getState() instanceof InventoryHolder) {
+
+				InventoryHolder invh = (InventoryHolder) e.getClickedBlock().getState();
+				Inventory inv = invh.getInventory();
+
+				ItemStack[] contents = inv.getContents();
+				for (int i = 0; i < contents.length; ++i)
+					if (contents[i] != null) contents[i] = contents[i].clone();
+
+				Inventory newinv;
+				if (inv instanceof DoubleChestInventory)
+					newinv = Bukkit.getServer().createInventory(null, 54, "Large Chest");
+				else newinv = Bukkit.getServer().createInventory(null, inv.getType());
+				newinv.setContents(contents);
+
+				e.getPlayer().openInventory(newinv);
+				e.setCancelled(true);
+				return;
+			}
+			// Cancel all other actions
+			e.setCancelled(true);
+		}
+	}
+	
+	
 }
