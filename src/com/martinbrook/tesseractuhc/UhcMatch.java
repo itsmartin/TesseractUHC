45e6a7177c4a56636e4fa5efb002147148be8eea
itsmartin <github@martinbrook.com>
2012-11-29 17:08:14 +0000

    Removed unusued function (already moved to MatchUtils)

--- src/com/martinbrook/tesseractuhc/UhcMatch.java
+++ src/com/martinbrook/tesseractuhc/UhcMatch.java
@@ -29,9 +29,7 @@ import org.bukkit.entity.MagmaCube;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Slime;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
@@ -1324,41 +1322,7 @@ public class UhcMatch {
 
 
 
-	/**
-	 * Gets a copy of a player's current inventory, including armor/health/hunger details.
-	 *
-	 * @author AuthorBlues
-	 * @param player The player to be viewed
-	 * @return inventory The player's inventory
-	 *
-	 */
-	public Inventory getInventoryView(Player player)
-	{
 
-		PlayerInventory pInventory = player.getInventory();
-		Inventory inventoryView = Bukkit.getServer().createInventory(null,
-			pInventory.getSize() + 9, player.getDisplayName() + "'s Inventory");
-
-		ItemStack[] oldContents = pInventory.getContents();
-		ItemStack[] newContents = inventoryView.getContents();
-
-		for (int i = 0; i < oldContents.length; ++i)
-			if (oldContents[i] != null) newContents[i] = oldContents[i];
-
-		newContents[oldContents.length + 0] = pInventory.getHelmet();
-		newContents[oldContents.length + 1] = pInventory.getChestplate();
-		newContents[oldContents.length + 2] = pInventory.getLeggings();
-		newContents[oldContents.length + 3] = pInventory.getBoots();
-
-		newContents[oldContents.length + 7] = new ItemStack(Material.APPLE, player.getHealth());
-		newContents[oldContents.length + 8] = new ItemStack(Material.COOKED_BEEF, player.getFoodLevel());
-
-		for (int i = 0; i < oldContents.length; ++i)
-			if (newContents[i] != null) newContents[i] = newContents[i].clone();
-
-		inventoryView.setContents(newContents);
-		return inventoryView;
-	}
 	
 
 	/**
