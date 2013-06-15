a110df7cc816aa4a331b1605e13e52e415dc2b3e
itsmartin <github@martinbrook.com>
2013-06-15 20:55:15 +0100

    Fix indentation (no code changes)

--- src/com/martinbrook/tesseractuhc/listeners/SpectateListener.java
+++ src/com/martinbrook/tesseractuhc/listeners/SpectateListener.java
@@ -30,7 +30,7 @@ import com.martinbrook.tesseractuhc.UhcPlayer;
 public class SpectateListener implements Listener {
 	private UhcMatch m;
 	public SpectateListener(UhcMatch m) { this.m = m; }
-	
+
 	@EventHandler
 	public void onInteractEntityEvent(PlayerInteractEntityEvent e) {
 		Player p = e.getPlayer();
@@ -43,28 +43,28 @@ public class SpectateListener implements Listener {
 			if (m.getPlayer((Player) clicked).isActiveParticipant())
 				pl.getSpectator().showInventory((Player) clicked);
 		}
-                
-                if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
+
+		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
 	}
-        
-        @EventHandler(ignoreCancelled = true)
-        public void onProjectileLaunch(ProjectileLaunchEvent e) {
-            Entity launcher = e.getEntity().getShooter();
-            if (launcher != null && launcher.getType() == EntityType.PLAYER) {
-                if (m.getPlayer((Player) launcher).isSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
-            }
-        }
-        
-        @EventHandler(ignoreCancelled = true)
-        public void onPlayerBucketEmpty(PlayerBucketEmptyEvent e) {
-            if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
-        }        
-        
-        @EventHandler(ignoreCancelled = true)
-        public void onPlayerBucketFill(PlayerBucketFillEvent e) {
-            if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
-        }
-	
+
+	@EventHandler(ignoreCancelled = true)
+	public void onProjectileLaunch(ProjectileLaunchEvent e) {
+		Entity launcher = e.getEntity().getShooter();
+		if (launcher != null && launcher.getType() == EntityType.PLAYER) {
+			if (m.getPlayer((Player) launcher).isSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onPlayerBucketEmpty(PlayerBucketEmptyEvent e) {
+		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
+	}        
+
+	@EventHandler(ignoreCancelled = true)
+	public void onPlayerBucketFill(PlayerBucketFillEvent e) {
+		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
+	}
+
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerPickupItem(PlayerPickupItemEvent e) {
 		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
@@ -74,15 +74,15 @@ public class SpectateListener implements Listener {
 	public void onPlayerDropItem(PlayerDropItemEvent e) {
 		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
 	}
-        
-        @EventHandler(ignoreCancelled = true)
-        public void onVehicleDamage(VehicleDamageEvent e) {
-            Entity attacker = e.getAttacker();
-            if (attacker != null && attacker.getType() == EntityType.PLAYER) {
-                	if (m.getPlayer((Player) attacker).isSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
-        	}
-        }
-        
+
+	@EventHandler(ignoreCancelled = true)
+	public void onVehicleDamage(VehicleDamageEvent e) {
+		Entity attacker = e.getAttacker();
+		if (attacker != null && attacker.getType() == EntityType.PLAYER) {
+			if (m.getPlayer((Player) attacker).isSpectator() && m.getMatchPhase() == MatchPhase.MATCH) e.setCancelled(true);
+		}
+	}
+
 	@EventHandler(ignoreCancelled = true)
 	public void onEntityTarget(EntityTargetEvent e) {
 		Entity target = e.getTarget();
@@ -94,58 +94,58 @@ public class SpectateListener implements Listener {
 	 * Prevent ops from interacting with anything when match is in progress.
 	 * 
 	 */
-	@EventHandler(ignoreCancelled = true)
+	 @EventHandler(ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) {
+		 if (m.getPlayer(e.getPlayer()).isNonInteractingSpectator() && m.getMatchPhase() == MatchPhase.MATCH) {
 
-			// Handle right-clicks on inventory blocks
-			if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getState() instanceof InventoryHolder) {
+			 // Handle right-clicks on inventory blocks
+			 if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getState() instanceof InventoryHolder) {
 
-				InventoryHolder invh = (InventoryHolder) e.getClickedBlock().getState();
-				Inventory inv = invh.getInventory();
+				 InventoryHolder invh = (InventoryHolder) e.getClickedBlock().getState();
+				 Inventory inv = invh.getInventory();
 
-				ItemStack[] contents = inv.getContents();
-				for (int i = 0; i < contents.length; ++i)
-					if (contents[i] != null) contents[i] = contents[i].clone();
+				 ItemStack[] contents = inv.getContents();
+				 for (int i = 0; i < contents.length; ++i)
+					 if (contents[i] != null) contents[i] = contents[i].clone();
 
-				Inventory newinv;
-				if (inv instanceof DoubleChestInventory)
-					newinv = Bukkit.getServer().createInventory(null, 54, "Large Chest");
-				else newinv = Bukkit.getServer().createInventory(null, inv.getType());
-				newinv.setContents(contents);
+				 Inventory newinv;
+				 if (inv instanceof DoubleChestInventory)
+					 newinv = Bukkit.getServer().createInventory(null, 54, "Large Chest");
+				 else newinv = Bukkit.getServer().createInventory(null, inv.getType());
+				 newinv.setContents(contents);
 
-				e.getPlayer().openInventory(newinv);
-				e.setCancelled(true);
-				return;
-			}
-			// Cancel all other actions
-			e.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(ignoreCancelled = true)
-	public void onBlockPlace(BlockPlaceEvent e) {
-		UhcPlayer pl = m.getPlayer(e.getPlayer());
-		if (pl.isAdmin()) return; // Ignore this event for admins.
-		
-		// Cancel the event if player is a spec, or match hasn't started
-		if (pl.isNonInteractingSpectator()
-				|| (m.getMatchPhase() == MatchPhase.PRE_MATCH || m.getMatchPhase() == MatchPhase.LAUNCHING)) {
-			e.setCancelled(true);
-			return;
-		}
-	}
-	
-	@EventHandler(ignoreCancelled = true)
-	public void onBlockBreak(BlockBreakEvent e) {
-		UhcPlayer pl = m.getPlayer(e.getPlayer());
-		if (pl.isAdmin()) return; // Ignore this event for admins.
-		
-		// Cancel the event if player is a spec, or match hasn't started
-		if (pl.isNonInteractingSpectator()
-				|| (m.getMatchPhase() == MatchPhase.PRE_MATCH || m.getMatchPhase() == MatchPhase.LAUNCHING)) {
-			e.setCancelled(true);
-			return;
-		}
-	}
+				 e.getPlayer().openInventory(newinv);
+				 e.setCancelled(true);
+				 return;
+			 }
+			 // Cancel all other actions
+			 e.setCancelled(true);
+		 }
+	 }
+
+	 @EventHandler(ignoreCancelled = true)
+	 public void onBlockPlace(BlockPlaceEvent e) {
+		 UhcPlayer pl = m.getPlayer(e.getPlayer());
+		 if (pl.isAdmin()) return; // Ignore this event for admins.
+
+		 // Cancel the event if player is a spec, or match hasn't started
+		 if (pl.isNonInteractingSpectator()
+				 || (m.getMatchPhase() == MatchPhase.PRE_MATCH || m.getMatchPhase() == MatchPhase.LAUNCHING)) {
+			 e.setCancelled(true);
+			 return;
+		 }
+	 }
+
+	 @EventHandler(ignoreCancelled = true)
+	 public void onBlockBreak(BlockBreakEvent e) {
+		 UhcPlayer pl = m.getPlayer(e.getPlayer());
+		 if (pl.isAdmin()) return; // Ignore this event for admins.
+
+		 // Cancel the event if player is a spec, or match hasn't started
+		 if (pl.isNonInteractingSpectator()
+				 || (m.getMatchPhase() == MatchPhase.PRE_MATCH || m.getMatchPhase() == MatchPhase.LAUNCHING)) {
+			 e.setCancelled(true);
+			 return;
+		 }
+	 }
 }
