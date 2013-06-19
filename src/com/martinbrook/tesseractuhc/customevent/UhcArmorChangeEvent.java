8d825ef68f467c7ab4d332ad0200bdb94e9f5722
itsmartin <github@martinbrook.com>
2013-06-19 12:53:51 +0100

    Added a Player for health and armor events

--- src/com/martinbrook/tesseractuhc/customevent/UhcArmorChangeEvent.java
+++ src/com/martinbrook/tesseractuhc/customevent/UhcArmorChangeEvent.java
@@ -1,21 +1,27 @@
 package com.martinbrook.tesseractuhc.customevent;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import com.martinbrook.tesseractuhc.UhcMatch;
 
 public class UhcArmorChangeEvent extends UhcMatchEvent {
 
 	private int armorPoints;
+	private Player player;
 
-	public UhcArmorChangeEvent(UhcMatch match, Location location, int armorPoints) {
+	public UhcArmorChangeEvent(UhcMatch match, Location location, Player player, int armorPoints) {
 		super(match, location);
 		this.armorPoints = armorPoints;
+		this.player = player;
 	}
 
 	public int getArmorPoints() {
 		return armorPoints;
 	}
-
+	
+	public Player getPlayer() {
+		return player;
+	}
 	
 }
--- src/com/martinbrook/tesseractuhc/customevent/UhcHealthChangeEvent.java
+++ src/com/martinbrook/tesseractuhc/customevent/UhcHealthChangeEvent.java
@@ -1,22 +1,28 @@
 package com.martinbrook.tesseractuhc.customevent;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import com.martinbrook.tesseractuhc.UhcMatch;
 
 public class UhcHealthChangeEvent extends UhcMatchEvent {
 
 	private int health;
+	private Player player;
 	
-	public UhcHealthChangeEvent(UhcMatch match, Location location, int health) {
+	public UhcHealthChangeEvent(UhcMatch match, Location location, Player player, int health) {
 		super(match, location);
 		this.health = health;
+		this.player = player;
 	}
 
 
 	public int getHealth() {
 		return health;
 	}
-
+	
+	public Player getPlayer() {
+		return player;
+	}
 	
 }
